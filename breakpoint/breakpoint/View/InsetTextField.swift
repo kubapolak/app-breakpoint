332e0f80da22df015038496761ca61cad6698217
Mac <mac@pc11.home>
2017-10-15 19:14:45 +0200

    finished login UIs

---
+++ breakpoint/breakpoint/View/InsetTextField.swift
@@ -0,0 +1,36 @@
+//
+//  InsetTextField.swift
+//  breakpoint
+//
+//  Created by Mac on 10/15/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class InsetTextField: UITextField {
+
+    private var padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+    
+    
+    override func awakeFromNib() {
+        let placeholder = NSAttributedString(string: self.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 0.8133803456, green: 1, blue: 0.9995977238, alpha: 1)])
+        
+        self.attributedPlaceholder = placeholder
+        
+        super.awakeFromNib()
+    }
+    
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+}
