872fed7d855f62bd3c66269473883cbad871b7d1
Mac <mac@pc11.home>
2017-10-24 19:28:29 +0200

    user cell created

--- breakpoint/breakpoint/Controller/CreateGroupsVC.swift
+++ breakpoint/breakpoint/Controller/CreateGroupsVC.swift
@@ -25,11 +25,29 @@ class CreateGroupsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     @IBAction func doneButtonPressed(_ sender: Any) {
     }
     
     @IBAction func closeButtonPressed(_ sender: Any) {
+    }
+}
+
+
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else { return UITableViewCell() }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        cell.configureCell(profileImage: profileImage!, email: "rick@morty.com", isSelected: true)
+        return cell
     }
 }
